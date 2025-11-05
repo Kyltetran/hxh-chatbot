a95a6a93f9d30d910d1864aee9708af05915d403
Kyltetran <138639043+Kyltetran@users.noreply.github.com>
2025-11-06 00:30:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def health_check():
 
 # deploy
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 # run local
 # if __name__ == "__main__":
